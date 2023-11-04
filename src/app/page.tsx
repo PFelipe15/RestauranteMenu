74077a810d28c7b3e294ecd8c36351199691cf70
Paulo Felipe <PauloFelipet15@gmail.com>
2023-11-04 19:15:22 -0300

    new modified in feature Image Ampliar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -114,6 +114,7 @@ export default function Home() {
             Fechar
           </button>
           <div className="image-container grid grid-cols-3 gap-4">
+            {ampliarImageIndex !== null && <AmpliarModal />}
             {currentGallery &&
               currentGallery.map((image, index) => (
                 <div
@@ -145,7 +146,7 @@ export default function Home() {
     }
 
     return (
-      <div className="fixed inset-0 z-60 flex items-center justify-center bg-black bg-opacity-50">
+      <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
         <div className="bg-white p-4 rounded-lg shadow-md">
           <button
             className="absolute top-2 right-2 text-gray-500 hover:text-gray-700"
@@ -235,7 +236,6 @@ export default function Home() {
               Pedir Orçamento
             </button>
             {isGalleryOpen && <ImageGallery />}
-            {ampliarImageIndex !== null && <AmpliarModal />}
 
             {isDialogOpen && (
               <OrcamentoDialog
