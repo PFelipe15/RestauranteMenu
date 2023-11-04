246f9a9dba464815f312e45609fac9cea2903d5e
Paulo Felipe <PauloFelipet15@gmail.com>
2023-11-04 15:44:34 -0300

    init

---
+++ src/app/components/OrcamentoModal.tsx
@@ -0,0 +1,120 @@
+import React, { useState } from "react";
+import Modal from "react-modal";
+
+export default function OrcamentoDialog(productName: string) {
+  const [hasMeasurements, setHasMeasurements] = useState(true);
+  const [userLocation, setUserLocation] = useState("");
+  const [largura, setLargura] = useState("");
+  const [comprimento, setComprimento] = useState("");
+
+  const handleConfirmOrcamento = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const { latitude, longitude } = position.coords;
+        const url = `http://nominatim.openstreetmap.org/reverse?format=json&lat=${latitude}&lon=${longitude}&addressdetails=1&accept-language=pt-BR&zoom=18`;
+
+        fetch(url)
+          .then(function (response) {
+            return response.json();
+          })
+          .then(function (data) {
+            alert(data.display_name);
+          })
+          .catch(function (error) {
+            console.error(error);
+          });
+      });
+    } else {
+      alert("A geolocalização não é suportada neste navegador.");
+    }
+
+    let metragem = "";
+
+    if (hasMeasurements) {
+      if (largura && comprimento) {
+        const total = Number(largura) + Number(comprimento);
+        metragem = `Minhas metragens são Largura: ${largura} x Comprimento: ${comprimento}, dando um total de ${total} metros quadrados.`;
+      } else {
+        alert("Por favor, preencha os valores de largura e comprimento.");
+        return;
+      }
+    }
+
+    const productInfo = `Quero fazer um orçamento de ${productName}, moro em ${userLocation}. ${metragem}`;
+    console.log(productInfo);
+    const link = `https://api.whatsapp.com/send?phone=5586988034600&text=Olá, ${productInfo}`;
+
+    // window.location.href = link;
+  };
+
+  return (
+    <Modal
+      isOpen={true}
+      onRequestClose={() => {}}
+      contentLabel="Confirmação de Orçamento"
+      className="bg-white border border-gray-300 p-4 rounded-md shadow-lg max-w-sm mx-auto"
+      overlayClassName="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-50"
+    >
+      <h2 className="text-2xl font-semibold">Confirmação de Orçamento</h2>
+      <p className="text-gray-600">
+        Deseja informar as medidas da metragem para o orçamento?
+      </p>
+      <div className="flex space-x-4 mt-4">
+        <button
+          onClick={() => setHasMeasurements(true)}
+          className={`${
+            hasMeasurements
+              ? "bg-blue-500 text-white"
+              : "bg-gray-300 text-gray-600"
+          } px-4 py-2 rounded-md hover:bg-blue-600`}
+        >
+          Sim
+        </button>
+        <button
+          onClick={() => setHasMeasurements(false)}
+          className={`${
+            !hasMeasurements
+              ? "bg-blue-500 text-white"
+              : "bg-gray-300 text-gray-600"
+          } px-4 py-2 rounded-md hover:bg-blue-600`}
+        >
+          Não
+        </button>
+      </div>
+
+      <div>
+        {hasMeasurements ? (
+          <div className="flex mt-3  items-center justify-evenly">
+            <div className="flex flex-col items-center">
+              <input
+                className="bg-white text-blue-500 w-24 py-2 rounded-md border border-blue-600"
+                type="number"
+                value={largura}
+                onChange={(e) => setLargura(e.target.value)}
+              />
+              <span>Largura</span>
+            </div>
+            <p>x</p>
+            <div className="flex flex-col items-center">
+              <input
+                className="bg-white text-blue-500 w-24 py-2 rounded-md border border-blue-600"
+                type="number"
+                value={comprimento}
+                onChange={(e) => setComprimento(e.target.value)}
+              />
+              <span>Comprimento</span>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
+      </div>
+      <button
+        onClick={handleConfirmOrcamento}
+        className="mt-4 bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 w-full"
+      >
+        Confirmar
+      </button>
+    </Modal>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,128 @@
-import Image from 'next/image'
+"use client";
+import Image from "next/image";
+import imagem from "../assets/banner.jpg";
+import Logo from "../assets/logo.png";
+import { FaMapMarkerAlt, FaPhone, FaEnvelope } from "react-icons/fa";
+import { FaFacebook, FaInstagram, FaTwitter } from "react-icons/fa";
+import { useState } from "react";
+import OrcamentoDialog from "./components/OrcamentoModal";
 
 export default function Home() {
+  const [productName, setProductName] = useState("");
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+
+  const produtosWRGESSO = [
+    {
+      nome: "Forro de Gesso (por metro)",
+      preco: 49.99,
+      imagem: imagem,
+      descricao:
+        "Forro de gesso de alta qualidade para transformar seus tetos com elegância e sofisticação.",
+    },
+    {
+      nome: "Sancas de Gesso (por metro)",
+      preco: 29.99,
+      imagem: imagem,
+      descricao:
+        "Sancas de gesso elegantes para adicionar um toque de estilo aos seus ambientes.",
+    },
+    {
+      nome: "Divisórias de Gesso (por metro)",
+      preco: 99.99,
+      imagem: imagem,
+      descricao:
+        "Divisórias de gesso versáteis para criar espaços funcionais em seu interior.",
+    },
+    {
+      nome: "Molduras de Gesso (por metro)",
+      preco: 19.99,
+      imagem: imagem,
+      descricao:
+        "Molduras de gesso decorativas para realçar a beleza de suas paredes e tetos.",
+    },
+    // Adicione mais produtos conforme necessário
+  ];
+
+  const handlePedirOrcamento = async (nomeProduto: string) => {
+    setProductName(nomeProduto);
+    setIsDialogOpen(true);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex min-h-screen flex-col">
+      <header className="bg-blue-500 p-4 shadow-md">
+        <div className="container mx-auto flex items-center justify-between">
+          <div className="flex items-center space-x-3">
+            <Image alt="logo" src={Logo} width={80} height={80} />
+            <div className="text-white">
+              <h1 className="text-2xl font-semibold">WR GESSO</h1>
+              <p className="text-sm">Gesso de Excelência, Teto de Elegância.</p>
+            </div>
+          </div>
+          <div className="text-white flex items-center space-x-4">
+            <a href="#" className="hover:text-gray-300">
+              <FaFacebook size={24} />
+            </a>
+            <a href="#" className="hover:text-gray-300">
+              <FaInstagram size={24} />
+            </a>
+            <a href="#" className="hover:text-gray-300">
+              <FaTwitter size={24} />
+            </a>
+          </div>
+        </div>
+      </header>
+
+      <section className="container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 p-4">
+        {produtosWRGESSO.map((produto, index) => (
+          <div
+            key={index}
+            className="bg-white p-4 rounded-lg shadow-md border-2 border-blue-200 flex flex-col"
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            <div className="flex items-center justify-center">
+              <Image
+                alt="produto imagem"
+                src={produto.imagem}
+                width={300}
+                height={200}
+              />
+            </div>
+            <h2 className="text-lg font-semibold mt-2">{produto.nome}</h2>
+            <p className="text-gray-600 text-sm">{produto.descricao}</p>
+            <div className="flex justify-between items-center mt-4">
+              <p className="text-xl font-semibold text-blue-500">
+                R$ {produto.preco.toFixed(2)} / metro
+              </p>
+              <a
+                href="#"
+                className="bg-blue-900 text-white px-2 py-1 rounded-md"
+              >
+                Imagens
+              </a>
+            </div>
+            <button
+              onClick={() => {
+                handlePedirOrcamento(produto.nome);
+              }}
+              className="bg-blue-500 w-full text-white px-8 py-2 rounded-md mt-4"
+            >
+              Pedir Orçamento
+            </button>
+
+            {isDialogOpen && <OrcamentoDialog productName={productName} />}
+          </div>
+        ))}
+      </section>
+
+      <div className="bg-white p-4 border-t border-gray-200 flex justify-between items-center flex-col">
+        <div className="flex items-center p-2">
+          <FaMapMarkerAlt className="text-blue-500 mr-3 " size={24} />
+          <p className="text-blue-500">
+            Rua Adão Belarmino, 1992 - Parque Piauí II, Timon - MA, 65636-540,
+            Brasil
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
